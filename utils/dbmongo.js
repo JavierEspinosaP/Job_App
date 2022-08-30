f615daeb2db46b5475f616d16ed97ee4c768bc79
Javier Espinosa <javierespinosapasamontes@gmail.com>
2022-08-30 16:23:23 +0200

    first commit

--- utils/dbmongo.js
+++ utils/dbmongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterjobapp.l2mk52w.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, ssl: true});
+mongoose.connect(`mongodb+srv://${process.env.DB_USER_MONGODB}:${process.env.DB_PASSWORD_MONGODB}@clusterjobapp.l2mk52w.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, ssl: true});
 const db = mongoose.connection;
 
 // Eventos
