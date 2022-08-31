15bd0a2b5f73388a70c946adad40ada5fa7451f3
Javier Espinosa <javierespinosapasamontes@gmail.com>
2022-08-31 21:52:50 +0200

    home_navigation_done

---
+++ public/scripts/homeNavigation.js
@@ -0,0 +1,33 @@
+//Selectores
+
+const signUpAccess = document.getElementById('signUpAccess')
+const mainContainer = document.getElementById('mainContainer')
+const signUpContainer = document.getElementById('signUpContainer')
+const comeBackButton = document.getElementById('comeBackButton')
+const loginAccess = document.getElementById('loginAccess')
+const loginContainer = document.getElementById('loginContainer')
+const comeBackButton2 = document.getElementById('comeBackButton2')
+
+
+
+//Navegacion
+
+signUpAccess.addEventListener('click', ()=>{
+    mainContainer.style.display = 'none'
+    signUpContainer.style.display = 'block'
+})
+
+comeBackButton.addEventListener('click', ()=>{
+    mainContainer.style.display = 'block'
+    signUpContainer.style.display = 'none'
+})
+
+loginAccess.addEventListener('click', ()=>{
+    mainContainer.style.display = 'none'
+    loginContainer.style.display = 'flex'
+})
+
+comeBackButton2.addEventListener('click', ()=>{
+    mainContainer.style.display = 'block'
+    loginContainer.style.display = 'none'
+})
