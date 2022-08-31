44a60ac01d702db4f6361a5aca26cada998650de
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-08-31 14:44:32 +0200

    añadidas vistas de prueba

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-const express = require('express')
-
+// Módulos externos
 require('./utils/dbmongo');
 require('./utils/db_sql');
+const express = require('express')
 const cowsay = require('cowsay2');
 const owl = require('cowsay2/cows/owl');
 
-
 //routes
-
 const userRoutes = require('./routes/usersApiRoutes')
 
-
-//Middleware 404
+//Middlewares
 const manage404 = require('./middlewares/error404')
-
 const app = express()
 const cors = require("cors");
 const port = 3005
 
+// View engine
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 //Read body from request
-
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-
 //ENDPOINTS
-app.use('/', frontRoutes)
-app.use('/users', userRoutes)
-
+//WEB
+// app.use('/', frontRoutes);
+app.use('/users', userRoutes);
 
 //If routes fail, show error 404
 app.use(manage404);
-
 //Owl say you if server works
 app.listen(port, () => {
-    console.log(cowsay.say(`Server working on http://localhost:${port}`, { cow: owl }))
-  });
+  console.log(cowsay.say(`Server working on http://localhost:${port}`, { cow: owl }))
+});
