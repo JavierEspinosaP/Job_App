b66414d978837ceb660e21b69a9da894699ecba0
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-09-01 15:55:31 +0200

    ADD/sqlLocal modified

--- utils/db_sql.js
+++ utils/db_sql.js
@@ -17,38 +17,21 @@ const pool = new Pool({
 module.exports = client
 
 //SQL en la nube
-async function connect (client) {
-  try {
-    await client.connect()
-    console.log('Client connected')
+// async function connect (client) {
+//   try {
+//     await client.connect()
+//     console.log('Client connected')
 
-    const {rows} = await client.query('SELECT * FROM users')
-    console.table(rows)
-    await client.end()
-  }
-  catch (ex){
-    console.log("Some error" +  ex);
-  }
-
-finally {
-  await client.end()
-}
-}
-connect (client)
-
-// let postgressUrl = "postgres://asmlztcc:m_gSxwvBV_YOoJw_bbWfGRjYY6J5i8MY@tyke.db.elephantsql.com/asmlztcc" 
-// let users = new Pool(postgressUrl);
-
-// users.connect(function(err) {
-//   if(err) {
-//     return console.error('could not connect to postgres', err);
+//     const {rows} = await client.query('SELECT * FROM users')
+//     console.table(rows)
+//     await client.end()
 //   }
-//   users.query('SELECT NOW() AS "theTime"', function(err, result) {
-//     if(err) {
-//       return console.error('error running query', err);
-//     }
-//     console.log(result.rows[0].theTime);
-//     // >> output: 2018-08-23T14:02:57.117Z
-//     users.end();
-//   });
-// });
+//   catch (ex){
+//     console.log("Some error" +  ex);
+//   }
+
+// finally {
+//   await client.end()
+// }
+// }
+// connect (client)
