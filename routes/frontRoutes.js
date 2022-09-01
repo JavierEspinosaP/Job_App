b7be52ad375fe94c919dcf773251688f22173fa2
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-09-01 09:58:03 +0200

    añadidas todas las vistas

--- routes/frontRoutes.js
+++ routes/frontRoutes.js
@@ -3,5 +3,12 @@ const frontController = require("../controllers/frontControllers");
 const frontRouter = express.Router();
 
 frontRouter.get('/', frontController.getHome);
+frontRouter.get('/singup', frontController.getSingup);
+frontRouter.get('/login', frontController.getLogin);
+frontRouter.get('/favorites', frontController.getFavorites);
+frontRouter.get('/profile', frontController.getProfile);
+//Vistas solo de admin
+frontRouter.get('/users', frontController.getUsers);
+frontRouter.get('/dashboard', frontController.getDashboard);
 
 module.exports = frontRouter;
