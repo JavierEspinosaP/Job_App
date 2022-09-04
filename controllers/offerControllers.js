f03120072d24780b2441f0934f9f7b6ec5cc3efa
Javier Espinosa <javierespinosapasamontes@gmail.com>
2022-09-04 12:38:19 +0200

    working on scraper

--- controllers/offerControllers.js
+++ controllers/offerControllers.js
@@ -3,13 +3,30 @@ const scraper = require('../utils/scraper')
 
 async function getOffers (req, res) {
     try {
-        const offers = await scraper.scrap("https://www.workana.com/jobs?language=en%2Ces")
-        // const offers2= await scraper.scrap2("https://es.indeed.com/m")
-        res.status(200).json(offers)
+        // const offers1 = await scraper.scrap("https://www.workana.com/jobs?language=en%2Ces")
+        const offers2= await scraper.scrap2("https://www.freelancer.com/jobs/web-development/")
+        console.log(offers2);
+        res.status(200).json(offers2)
     }
     catch (err) {
         res.status(404).json({})
     }
 }
 
-module.exports = {getOffers} 
+// async function getOffers2 (req, res) {
+//     try {
+//         const offers1 = await scraper.scrap("https://www.workana.com/jobs?language=en%2Ces")
+
+//         res.status(200).json(offers1)
+//     }
+//     catch (err) {
+//         res.status(404).json({})
+//     }
+// }
+
+module.exports = {
+    getOffers,
+    // getOffers2
+} 
+
+///ITERAR LAS URLS EN EL SCRAPER PARA COGER AMBAS WEBS --> https://stackoverflow.com/questions/63580250/scrape-multiple-websites-using-puppeteer
--- utils/scraper.js
+++ utils/scraper.js
@@ -54,6 +54,9 @@ const scrap = async (url) => {
         }
         
         browser.close()
+
+       
+
         return scrapedData
     }
     catch (err) {
@@ -61,58 +64,65 @@ const scrap = async (url) => {
     }
 }
 
-// const scrap2 = async (url) => {
-//     try {
-//         //Creamos objeto vacío para almacenar la info de la oferta
+const scrap2 = async (url2) => {
+    try {
+        //Creamos objeto vacío para almacenar la info de la oferta
 
-//         const offerData = {} //Objeto de la oferta
-//         const scrapedData = [] //Array de los objetos de las ofertas
+        let offerData = {} //Objeto de la oferta
+        const scrapedData = [] //Array de los objetos de las ofertas
 
-//         //Inicializamos una instancia del navegador (browser) con puppeteer.launch() y añadimos en el objeto de configuración la opción headless
-//         console.log("Opening the browser...");
-//         const browser = await puppeteer.launch({headless:false})
+        //Inicializamos una instancia del navegador (browser) con puppeteer.launch() y añadimos en el objeto de configuración la opción headless
+        console.log("Opening the browser...");
+        const browser2 = await puppeteer.launch({headless:false})
 
-//         //Abrimos una nueva pestaña en el navegador creando una instancia con el método newPage() a la que llamaremos page
-//         const page = await browser.newPage();
-//         //Indicamos la url que debe cargarse en la pestaña con page.goto(url)
-//         await page.goto(url);
-//         console.log(`Navigating to ${url}...`);
+        //Abrimos una nueva pestaña en el navegador creando una instancia con el método newPage() a la que llamaremos page
+        const page2 = await browser2.newPage();
+        //Indicamos la url que debe cargarse en la pestaña con page.goto(url)
+        await page2.goto(url2);
+        console.log(`Navigating to ${url2}...`);
 
-//         //Extraemos todos los links a los que navegaremos para obtener la información de cada proyecto
+        //Extraemos todos los links a los que navegaremos para obtener la información de cada proyecto
 
-//         //Utilizamos el método $$eval(selector, callback) para capturar una colección de nodos y aplicar la lógica que necesitemos
+        //Utilizamos el método $$eval(selector, callback) para capturar una colección de nodos y aplicar la lógica que necesitemos
 
-//         /********** A RELLENAR page.$eval(selector, function)  *********/
+        /********** A RELLENAR page.$eval(selector, function)  *********/
+        let arrBudgetsClean = []
+
+        await page2.waitForSelector('#keyword-input', {visible: true}) // Esperamos a que aparezca el elemento
+        await page2.type('#keyword-input', searchQuery) //Introducimos la petición en el buscador
+        //Tiempo para que refresque la búsqueda
+        await page2.waitForTimeout(3000)
+        //Arrays con los datos de las ofertas
+        let arrNames2 = await page2.$$eval('.JobSearchCard-primary-heading-link', res => res.map(name=>name.innerText))
+        let arrPublished2 = await page2.$$eval('.JobSearchCard-primary-heading-days', res => res.map(date=>date.innerText))
+        let arrBudgets2 = await page2.$$eval('.JobSearchCard-primary-price', res => res.map(value=>value.innerText))
+        let arrUrls2 = await page2.$$eval('.JobSearchCard-primary-heading-link', res => res.map(url=>url.href))
+
+        for (let i = 0; i < arrBudgets2.length; i++) {
+            let clean = arrBudgets2[i].replace(/\n/g, ' ')
+            arrBudgetsClean.push(clean)
+        }
 
 
-//         await page.waitForSelector('#keywords-input', {visible: true}) // Esperamos a que aparezca el elemento
-//         await page.type('#keywords-input', searchQuery) //Introducimos la petición en el buscador
-//         await page.waitForTimeout(1000)//Tiempo para que refresque la búsqueda
+        //Bucle para formar cada objeto con los datos de cada oferta
+        for (let index = 0; index < arrNames2.length; index++) {
+            offerData = {}
+            offerData['proyect-name'] = arrNames2[index]
+            offerData['published'] = arrPublished2[index]
+            offerData['budget'] = arrBudgetsClean[index]
+            offerData['url'] = arrUrls2[index]
+            scrapedData.push(offerData)
+        }
+        
 
-//         //Arrays con los datos de las ofertas
-//         let arrNames = await page.$$eval('.job-title h2', res => res.map(title=>title.innerText))
-//         let arrCompanies = await page.$$eval('.companyName', res => res.map(comp=>comp.innerText))
-//         let arrDates = await page.$$eval('.date-field p', res => res.map(date=>date.innerText))
-//         let arrLocations = await page.$$eval('.companyLocation', res => res.map(date=>date.innerText))
-//         let arrUrls = await page.$$eval('.job-card-header a', res => res.map(url=>url.href))
-
-//         //Bucle para formar cada objeto con los datos de cada oferta
-//         for (let index = 0; index < arrNames.length; index++) {
-//             offerData['proyect-name'] = arrNames[index]
-//             offerData['company'] = arrCompanies[index]
-//             offerData['date'] = arrDates[index]
-//             offerData['location'] = arrLocations[index]
-//             offerData['url'] = arrUrls[index]
-//             scrapedData.push(offerData)
-//         }
-//         browser.close()
-//         return scrapedData
-//     }
-//     catch (err) {
-//         console.log("Error:", err);
-//     }
-// }
+        return scrapedData
+    }
+    catch (err) {
+        console.log("Error:", err);
+    }
+}
 
 module.exports = {
-    scrap
+    scrap,
+    scrap2
 }
