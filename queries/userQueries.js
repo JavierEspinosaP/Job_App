394574eb0da0f860d1bb6d2699a5067ef2d13c50
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-09-06 16:20:07 +0200

    prueba commit

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -6,8 +6,8 @@ const queries = {
     "updateStatus": `UPDATE users SET logged = true WHERE email = $1 AND logged = false`,
     "favOffers": `SELECT reference_offer FROM favorites INNER JOIN users ON users.user_id= favorites.user_id WHERE users.email = $1`, //traemos ofertas ya guardadas como favs
     "profile": `SELECT name, surname, email, role WHERE users.email = $1`,
-    "registerUser": `INSERT INTO users(name, surname, email, password,  role, logged) VALUES($1, $2, $3,$4, 'member', false)`,
-    "editDataProfile": `UPDATE users SET name = '$1', surname = '$2' WHERE email = $3`,
+    "registerUser": `INSERT INTO users(name, surname, email, password,  role, logged) VALUES($1, $2, $3, $4, 'member', false)`,
+    "editDataProfile": `UPDATE users SET name = '$1', surname = $2 WHERE email = $3`,
     "logoutUser": `UPDATE users SET logged = false WHERE email = $1 AND logged = true`,
     "saveFav": ``,
     "deleteFav": `DELETE FROM favorites WHERE reference_offer = $1`,
