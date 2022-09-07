3213bdba51b5545dde5fcbaa2d18500b1e373e27
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-09-07 17:05:33 +0200

    ADD/project cleared

--- routes/recoverPasswordApiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const passwordController = require("../controllers/passwordControllers")
-
-// [GET] /recoverpassword Recuperar password
-router.get("/",passwordController.recoverPassword)
-module.exports = router
