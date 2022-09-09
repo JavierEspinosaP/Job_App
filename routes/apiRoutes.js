8186c1b795a89aed4025f8ceecb40c3245eff6c8
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-09-09 13:25:15 +0200

    EDIT/ apiRoutes.js edited

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,10 +9,6 @@ const userController = require('../controllers/frontControllers');
 // router.post('/user', userController.signinUser);
 // router.put('/user', userController.editProfile);
 
-router.delete('/user/delete/', adminController.deleteUser); //Borrar un usuario de la base de datos (admin)
-// router.post('/favorites', userController.saveFavorite);
-// router.delete('/favorites', userController.deleteFavorite);
-
 //funciones MONGO (admin)
 router.post('/ads/create', adminController.createOffer);
 router.post('/ads/update/', adminController.updateOffer);
