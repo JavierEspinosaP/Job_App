e3c31c8eaf5b9bc60291e3a249f9c5d9a6e57dfe
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-09-12 19:53:23 +0200

    login_works

--- utils/db_sql.js
+++ utils/db_sql.js
@@ -1,14 +1,14 @@
 
-const pg = require('pg');
+const {Pool} = require('pg');
 require('dotenv').config()
 
 
 //ELEPHANT
 
-const pgUrl = process.env.DATABASE_URL
-const client = new pg.Client(pgUrl)
-let clientConnected = client.connect() //se conecta a la bd
-console.log("Estamos conectados a Elephant", clientConnected)
+// const pgUrl = process.env.DATABASE_URL
+// const client = new pg.Client(pgUrl)
+// let clientConnected = client.connect() //se conecta a la bd
+// console.log("Estamos conectados a Elephant", clientConnected)
 
 
 // const client = new ClientClass(pgUrl)
@@ -16,13 +16,13 @@ console.log("Estamos conectados a Elephant", clientConnected)
 // console.log("Estamos conectados a Elephant", clientConnected)
 
 // //LOCAL
-// const pool = new Pool({
-//   host: process.env.HOST_LOCAL,
-//   user: process.env.USER_LOCAL,
-//   database: process.env.DB_LOCAL,
-//   password: process.env.PASSWORD_LOCAL,
-//   port: process.env.PORT_LOCAL
-// })
+const pool = new Pool({
+  host: process.env.HOST_LOCAL,
+  user: process.env.USER_LOCAL,
+  database: process.env.DB_LOCAL,
+  password: process.env.PASSWORD_LOCAL,
+  port: process.env.PORT_LOCAL
+})
 
 
-module.exports = client
+module.exports = pool
