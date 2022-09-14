1199bf7e633ef2fe3bdacc232f9bc38a8dd8a152
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-09-14 17:45:59 +0200

    pool

--- models/users.js
+++ models/users.js
@@ -19,7 +19,7 @@ const registerUser = async (user) => {
         throw err;
     }
     finally{
-        client.release();
+        pool.release();
     }
     return result
 
@@ -37,7 +37,7 @@ const signInUser = async (user) => {
         console.log(err);
         throw err;
     } finally {
-        client.release();
+        pool.release();
     }
     return result
 }
@@ -53,7 +53,7 @@ const loggedStatus= async (email) => {
         console.log(err);
         throw err;
     } finally {
-        client.release();
+        pool.release();
     }
     return result
 }
